48f79ad3974ea840f338a9d6e6fda65600b38a43
MikleDev <justamazingemail@gmail.com>
2020-04-07 16:49:27 +0300

    add barChart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,8 +4,8 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
-
+const Chart = (props) => {
+  const { data: { recovered, confirmed, deaths }, country } = props;
   const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
@@ -35,14 +35,34 @@ const Chart = () => {
          }],
        }}
      />
-    ) : (
-      null
-    )
+    ) : null
+  );
+
+  const barChart = (
+    confirmed ? (
+      <Bar 
+        data = {{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [{
+            label: 'People',
+            data: [confirmed.value, recovered.value, deaths.value],
+            backgroundColor: [
+              'rgba(0, 0, 255, 0.5)',
+              'rgba(0, 255, 0, 0.5)',
+              'rgba(255, 0, 0, 0.5)'],
+          }]
+        }}
+        options = {{
+          legend: { display:false },
+          title: { display: true, text: `Current state in ${country}`}
+        }}
+      />
+    ) : null
   );
 
   return (
    <div className={styles.container}>
-     { lineChart }
+     { country ? barChart : lineChart }
    </div>
   );
 };
